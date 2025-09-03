05e700bf7b0b9a11edd0f546f14ae583af144ac2
Sarim <sarimjokhioprivate@gmail.com>
2025-09-03 16:46:08 +0500

    echo command

---
+++ hello.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#This is a basic script to print the stuff.
+echo "Hello World"
+echo "Bash Scripting: It's a scripting language used for automating tasks"
+echo "Script writers should choose vim to write because on remote servers there's no GUI, that's where vim is needed. So, better practice on vim from the beginning"
