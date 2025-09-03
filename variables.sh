e25c3b9c8bc9b5e21c8994539e467d804d6f0c64
Sarim <sarimjokhioprivate@gmail.com>
2025-09-03 17:31:34 +0500

    varaibles and user input

---
+++ variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+name="Sarim"
+age=23
+
+echo "My name is $name"
+echo "And my age is $age"
+
+# This is how you take user input in Bash.
+echo "Hey, enter your best friend's name"
+read friend_name
+echo "Good! Your best friend is: $friend_name"
