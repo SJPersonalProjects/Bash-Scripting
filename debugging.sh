ca5a937eb8816596d31e8ccea4062b516a13eb22
Sarim <sarimjokhioprivate@gmail.com>
2025-09-05 23:57:51 +0500

    Debugging

---
+++ debugging.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pwd
+date
+echo Hello
+echo "Bye"
+set -x
+echo "Today date is $(date)"
+echo "Hi your current directory is: $(pwd)"
+set +x
+echo "++++++++++++++++++++++++++++++++++++++++"
